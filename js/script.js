6b9904f9ba784330ad2bf41d7a4a07d39000b20c
Andrew S <sadulin2011@yandex.ru>
2017-03-15 05:16:05 +0300

    Bug fixing: data entering, information model

--- js/script.js
+++ js/script.js
@@ -5,72 +5,80 @@ const /* PRESET_BOX = document.getElementById('preset_fieldset'),
 
 	QUESTION_BOX = document.getElementById('question_fieldset'),
 	TARGET_VALUES = getInputFieldsByType(QUESTION_BOX, 'radio'),
+	
 	DEFAULT_TARGET_ID = 'transmitter_power',
 	DEFAULT_TARGET = document.getElementById(DEFAULT_TARGET_ID),
+	FIELD_DISABLED_BY_DEFAULT = document.getElementsByName(DEFAULT_TARGET_ID)[0],
 
 	PARAMETERS_BOX = document.getElementById('parameters_fieldset'),
 	PARAMETERS_FIELDS = getInputFieldsByType(PARAMETERS_BOX, 'number'),
 
-	FIELD_DISABLED_BY_DEFAULT = document.getElementsByName(DEFAULT_TARGET_ID)[0],
 	UNIT_SELECTORS = document.getElementsByClassName('unit_selector'),
 	WATT_UNIT = 'Вт', DB_UNIT = 'дБ', DBM_UNIT = 'дБм',
 
 	CLEAR_BUTTON = document.getElementsByName('reset_button')[0],
 	EXECUTE_BUTTON = document.getElementsByName('calculate_button')[0],
 
-	ALLOWED_PARAMETER_TEMPLATE = new RegExp('^-?[0-9]+[\\.,]?[0-9]*$'),
+	ALLOWED_PARAMETER_TEMPLATE = new RegExp('^-?[0-9]+\\.?[0-9]*$'),
 	WRONG_NUMBER_CLASS_NAME = 'wrong_number_format',
+	UNIT_POSTFIX = '_unit',
 
 	FORMULAS = {
+
 		'transmitter_power' : function() {
-			return -1 * (CONNECTION.transmitter_gain + 10 * log10(CONNECTION.transmitter_loss) - CONNECTION.reciever_power 
-				+ CONNECTION.reciever_gain + 10 * log10(CONNECTION.reciever_loss) + 20 * log10(300 / CONNECTION.main_frequency) 
-				- 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI));
+			return -1 * (CONNECTION.transmitter_gain.value + 10 * log10(CONNECTION.transmitter_loss.value) - CONNECTION.reciever_power.value 
+				+ CONNECTION.reciever_gain.value + 10 * log10(CONNECTION.reciever_loss.value) + 20 * log10(300 / CONNECTION.main_frequency.value) 
+				- 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI));
 		},
 		'transmitter_loss' : function() {
-			return Math.pow(10, -0.1 * (CONNECTION.transmitter_power + CONNECTION.transmitter_gain 
-				- CONNECTION.reciever_power + CONNECTION.reciever_gain + 10 * log10(CONNECTION.reciever_loss)
-				+ 20 * log10(300 / CONNECTION.main_frequency) - 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI)));
+			return Math.pow(10, -0.1 * (CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value 
+				- CONNECTION.reciever_power.value + CONNECTION.reciever_gain.value + 10 * log10(CONNECTION.reciever_loss.value)
+				+ 20 * log10(300 / CONNECTION.main_frequency.value) - 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI)));
 		},
 		'transmitter_gain' : function() {
-			return -1 * (CONNECTION.transmitter_power + 10 * log10(CONNECTION.transmitter_loss) - CONNECTION.reciever_power 
-				+ CONNECTION.reciever_gain + 10 * log10(CONNECTION.reciever_loss) + 20 * log10(300 / CONNECTION.main_frequency) 
-				- 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI));
+			return -1 * (CONNECTION.transmitter_power.value + 10 * log10(CONNECTION.transmitter_loss.value) - CONNECTION.reciever_power.value 
+				+ CONNECTION.reciever_gain.value + 10 * log10(CONNECTION.reciever_loss.value) + 20 * log10(300 / CONNECTION.main_frequency.value) 
+				- 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI));
 		},
 		'main_frequency' : function() {
-			return 300 / Math.pow(10, -0.05 * (CONNECTION.transmitter_power + CONNECTION.transmitter_gain + 10 * log10(CONNECTION.transmitter_loss) 
-				- CONNECTION.reciever_power + CONNECTION.reciever_gain + 10 * log10(CONNECTION.reciever_loss) 
-				- 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI)));
+			return 300 / Math.pow(10, -0.05 * (CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value + 10 * log10(CONNECTION.transmitter_loss.value) 
+				- CONNECTION.reciever_power.value + CONNECTION.reciever_gain.value + 10 * log10(CONNECTION.reciever_loss.value) 
+				- 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI)));
 		},
 		'reciever_power' : function() {
-			return CONNECTION.transmitter_power + CONNECTION.transmitter_gain + 10 * log10(CONNECTION.transmitter_loss) 
-				+ CONNECTION.reciever_gain + 10 * log10(CONNECTION.reciever_loss) + 20 * log10(300 / CONNECTION.main_frequency) 
-				- 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI);
+			return CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value + 10 * log10(CONNECTION.transmitter_loss.value) 
+				+ CONNECTION.reciever_gain.value + 10 * log10(CONNECTION.reciever_loss.value) + 20 * log10(300 / CONNECTION.main_frequency.value) 
+				- 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI);
 		},
 		'reciever_loss' : function() {
-			return Math.pow(10, -0.1 * (CONNECTION.transmitter_power + CONNECTION.transmitter_gain 
-				+ 10 * log10(CONNECTION.transmitter_loss) - CONNECTION.reciever_power + CONNECTION.reciever_gain 
-				+ 20 * log10(300 / CONNECTION.main_frequency) - 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI)));
+			return Math.pow(10, -0.1 * (CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value 
+				+ 10 * log10(CONNECTION.transmitter_loss.value) - CONNECTION.reciever_power.value + CONNECTION.reciever_gain.value 
+				+ 20 * log10(300 / CONNECTION.main_frequency.value) - 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI)));
 		},
 		'reciever_gain' : function() {
-			return -1 * (CONNECTION.transmitter_power + CONNECTION.transmitter_gain + 10 * log10(CONNECTION.transmitter_loss) 
-				- CONNECTION.reciever_power + 10 * log10(CONNECTION.reciever_loss) + 20 * log10(300 / CONNECTION.main_frequency) 
-				- 20 * log10(CONNECTION.max_range * 1000) - 20 * log10(4 * Math.PI));
+			return -1 * (CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value + 10 * log10(CONNECTION.transmitter_loss.value) 
+				- CONNECTION.reciever_power.value + 10 * log10(CONNECTION.reciever_loss.value) + 20 * log10(300 / CONNECTION.main_frequency.value) 
+				- 20 * log10(CONNECTION.max_range.value * 1000) - 20 * log10(4 * Math.PI));
 		},
 		'max_range' : function() {
-			return Math.pow(10, 0.05 * (CONNECTION.transmitter_power + CONNECTION.transmitter_gain 
-				+ 10 * log10(CONNECTION.transmitter_loss) - CONNECTION.reciever_power + CONNECTION.reciever_gain 
-				+ 10 * log10(CONNECTION.reciever_loss) + 20 * log10(300 / CONNECTION.main_frequency) - 20 * log10(4 * Math.PI))) / 1000;
+			return Math.pow(10, 0.05 * (CONNECTION.transmitter_power.value + CONNECTION.transmitter_gain.value 
+				+ 10 * log10(CONNECTION.transmitter_loss.value) - CONNECTION.reciever_power.value + CONNECTION.reciever_gain.value 
+				+ 10 * log10(CONNECTION.reciever_loss.value) + 20 * log10(300 / CONNECTION.main_frequency.value) - 20 * log10(4 * Math.PI))) / 1000;
 		}
 	};
 
 let WRONG_FILLED_FIELDS = document.getElementsByClassName(WRONG_NUMBER_CLASS_NAME),
 	CURRENT_DISABLED_FIELD = FIELD_DISABLED_BY_DEFAULT,
+	
 	CONNECTION = {
 		result_parameter : CURRENT_DISABLED_FIELD,
-		transmitter_unit : WATT_UNIT,
-		reciever_unit : WATT_UNIT
-	}
+		reciever_power : {
+			unit : WATT_UNIT
+		},
+		transmitter_power : {
+			unit : WATT_UNIT
+		}
+	};
 
 convertToArray(TARGET_VALUES).forEach(function(currentTarget) {
 	currentTarget.addEventListener('click', function() {
@@ -85,14 +93,16 @@ convertToArray(TARGET_VALUES).forEach(function(currentTarget) {
 		fieldToDisable.setAttribute('disabled', true);
 		CURRENT_DISABLED_FIELD = fieldToDisable;
 		CONNECTION.result_parameter = CURRENT_DISABLED_FIELD;
-		// console.log('Setted resut_parameter to ' + CONNECTION.result_parameter.name);
+		console.log('Setted resut_parameter to ' + CONNECTION.result_parameter.name);
 	});
 });
 
 convertToArray(PARAMETERS_FIELDS).forEach(function(currentField) {
 	currentField.addEventListener('blur', function() {
-		if (validateNumberField(this))
+		if (validateNumberField(this)) {
+			console.log('Field value successfully validated');
 			setSimpleConnectionParameter(this);
+		}
 	});
 	currentField.addEventListener('focus', function() {
 		this.classList.remove(WRONG_NUMBER_CLASS_NAME);
@@ -110,10 +120,10 @@ EXECUTE_BUTTON.addEventListener('click', function() {
 	if (correctlyFilledFields.length != PARAMETERS_FIELDS.length)
 		alert('Параметры заполнены некорректно');
 	else {
-		// console.log('Calculation started');
+		console.log('Calculation started');
 		convertPowerValuesBeforeCalculation();
-		let finalResult = convertPowerVariableAfterCalculation(+FORMULAS[CONNECTION.result_parameter.name]()).toFixed(2);
-		CONNECTION[CONNECTION.result_parameter.name] = CONNECTION.result_parameter.value = finalResult;
+		let finalResult = Number(FORMULAS[CONNECTION.result_parameter.name]())
+		CONNECTION.result_parameter.value = assignValueToObjectProperty([`${CONNECTION.result_parameter.name}`], 'value', convertPowerVariableAfterCalculation(finalResult));
 	}
 });
 
@@ -132,87 +142,88 @@ function convertToArray(nodeList) {
 	return Array.prototype.slice.call(nodeList);
 }
 
-function setSimpleConnectionParameter(currentField) {
-	let currentFieldName = currentField.name;
-	CONNECTION[currentFieldName] = (currentField.getAttribute('type') == 'number') ? +getValueAsNumber(currentField) : currentField.value;
-	// console.log('Setted ' + currentFieldName + ' to ' + CONNECTION[currentFieldName]);
-}
-
 function validateNumberField(field) {
-	let validationPassed = field.disabled || getValueAsNumber(field).length != 0 && ALLOWED_PARAMETER_TEMPLATE.test(getValueAsNumber(field));
-	if (!validationPassed) {
+	let validationPassed = field.disabled || ALLOWED_PARAMETER_TEMPLATE.test(field.value);
+	if (!validationPassed)
 		field.classList.add(WRONG_NUMBER_CLASS_NAME);
-	}
 	return validationPassed;
 }
 
-function getValueAsNumber(field) {
-	return (!isNaN(field.valueAsNumber)) ? field.valueAsNumber : field.value.replace(/,/, '.');
+function setSimpleConnectionParameter(currentField) {
+	let currentFieldName = currentField.name;
+	let currentParameterValue = (currentField.getAttribute('type') == 'number') ? getValueAsNumber(currentField) : currentField.value;
+	if (currentFieldName.indexOf(UNIT_POSTFIX) != -1) {
+		let relatedFieldName = getRelatedFieldName(currentField);
+		assignValueToObjectProperty(relatedFieldName, 'unit', currentParameterValue);
+		console.log('Setted ' + relatedFieldName + ' unit to ' + currentParameterValue);
+		return;
+	}
+	assignValueToObjectProperty(currentFieldName, 'value', currentParameterValue);
+	console.log('Setted ' + currentFieldName + ' value to ' + currentParameterValue);
 }
 
-// Should be refactored ASAP, such a shame
+function getValueAsNumber(field) {
+	return (!isNaN(field.valueAsNumber)) ? field.valueAsNumber : Number(field.value);
+}
+
+function getRelatedFieldName(currentSelector) {
+	return currentSelector.previousElementSibling.name;
+}
+
+function getRelatedUnitName(currentField) {
+	return currentField.nextElementSibling.value;
+}
+
+function assignValueToObjectProperty(fieldName, parameterName, value) {
+	if (CONNECTION.hasOwnProperty(fieldName))
+		CONNECTION[fieldName][`${parameterName}`] = value;
+	else
+		Object.defineProperty(CONNECTION, fieldName, {
+			value : {
+				[`${parameterName}`] : value
+			}
+		});
+	return value;
+}
+
 function convertPowerValuesBeforeCalculation() {
-	if (CONNECTION.result_parameter.name != 'transmitter_power') {
-		switch (CONNECTION.transmitter_unit) {
-			case WATT_UNIT : {
-				CONNECTION.transmitter_power = convertToDBMFromWatts(CONNECTION.transmitter_power);
-				// console.log('Converted transmitter_power unit from Watts to dBm: ' + CONNECTION.transmitter_power);
+	convertToArray(PARAMETERS_FIELDS).filter(function(currentField) {
+		return currentField.name != CONNECTION.result_parameter.name && currentField.name.indexOf('_power') != -1
+	}).forEach(function(currentElement) {
+		let currentElementName = currentElement.name;
+		switch (getRelatedUnitName(currentElement)) {
+			case WATT_UNIT: {
+				CONNECTION[`${currentElementName}`].value = convertToDBMFromWatts(getValueAsNumber(currentElement));
+				console.log('Converted ' + currentElementName + ' unit from Watts to dBm: ' + CONNECTION[`${currentElementName}`].value);
 				break;
 			}
-			case DB_UNIT : {
-				CONNECTION.transmitter_power = convertToDBMFromDB(CONNECTION.transmitter_power);
-				// console.log('Converted transmitter_power unit from dB to dBm: ' + CONNECTION.transmitter_power);
-				break;
-			}
-		}
-	}
-	if (CONNECTION.result_parameter.name != 'reciever_power') {
-		switch (CONNECTION.reciever_unit) {
-			case WATT_UNIT : {
-				// console.log(convertToDBMFromWatts(CONNECTION.reciever_power));
-				CONNECTION.reciever_power = convertToDBMFromWatts(CONNECTION.reciever_power);
-				// console.log('Converted reciever_power unit from Watts to dBm: ' + CONNECTION.reciever_power);
-				break;
-			}
-			case DB_UNIT : {
-				CONNECTION.reciever_power = convertToDBMFromDB(CONNECTION.reciever_power);
-				// console.log('Converted reciever_power unit from dB to dBm: ' + CONNECTION.reciever_power);
+			case DB_UNIT: {
+				CONNECTION[`${currentElementName}`].value = convertToDBMFromDB(getValueAsNumber(currentElement));
+				console.log('Converted ' + currentElementName + ' unit from dB to dBm: ' + CONNECTION[`${currentElementName}`].value);
 				break;
 			}
 		}
-	} 
+	});
 }
 
 function convertPowerVariableAfterCalculation(preCalculatedResult) {
-	// console.log('Before conversion: ' + preCalculatedResult);
-	if (CONNECTION.result_parameter.name == 'transmitter_power') {
-		switch (CONNECTION.transmitter_unit) {
-			case WATT_UNIT : {
+	convertToArray(PARAMETERS_FIELDS).filter(function(currentField) {
+		return currentField.name == CONNECTION.result_parameter.name && currentField.name.indexOf('_power') != -1
+	}).forEach(function(currentElement) {
+		let currentElementName = currentElement.name;
+		switch (getRelatedUnitName(currentElement)) {
+			case WATT_UNIT: {
 				preCalculatedResult = convertToWattsFromDBM(preCalculatedResult);
-				// console.log('Converted transmitter_power unit from dBm to Watts: ' + preCalculatedResult);
+				console.log('Converted ' + currentElementName + ' unit from dBm to Watts: ' + preCalculatedResult);
 				break;
 			}
-			case DB_UNIT : {
-				preCalculatedResult = convertToDBFromDBM(preCalculatedResult);
-				// console.log('Converted transmitter_power unit from dBm to dB: ' + preCalculatedResult);
+			case DB_UNIT: {
+				preCalculatedResult = convertToDBFromDBM(CONNECTION[`${currentElementName}`].value);
+				console.log('Converted ' + currentElementName + ' unit from dBm to DB: ' + preCalculatedResult);
 				break;
 			}
 		}
-	} else if (CONNECTION.result_parameter.name == 'reciever_power') {
-		switch (CONNECTION.reciever_unit) {
-			case WATT_UNIT : {
-				preCalculatedResult = convertToWattsFromDBM(preCalculatedResult);
-				// console.log('Converted reciever_power unit from dBm to Watts: ' + preCalculatedResult);
-				break;
-			}
-			case DB_UNIT : {
-				preCalculatedResult = convertToDBFromDBM(preCalculatedResult);
-				// console.log('Converted reciever_power unit from dBm to dB: ' + preCalculatedResult);
-				break;
-			}
-		}
-	}
-	// console.log('Converted: ' + preCalculatedResult);
+	});
 	return preCalculatedResult;
 }
 
